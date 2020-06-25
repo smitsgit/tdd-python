c661651f9c19608fe4e4709980112de6caa8973b
Smital Desai <desaismital@gmail.com>
2020-06-25 18:01:03 +0530

    First unit test and url mapping to dummy view

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,12 @@
 # Create your tests here.
-def test_bad_math():
-    assert 10 == 15, "Bad math"
+from django.urls import resolve
+from todolist.views import home_page
+
+
+# def test_bad_math():
+#     assert 10 == 15, "Bad math"
+
+def test_home_page_test():
+    found = resolve('/')
+    print(found.func)
+    assert found.func is home_page
