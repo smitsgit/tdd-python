33add4e45db0a77be44ca4fbab6b72c9c862d28c
Smital Desai <desaismital@gmail.com>
2020-06-26 15:15:20 +0530

    Get a failing test for adding second item

--- test/test_todo.py
+++ test/test_todo.py
@@ -42,14 +42,18 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_element_by_tag_name('tr')
-    assert any(row.text == '1: Buy peacock feathers' for row in rows), "New Item didn't appear in table"
+    check_for_row_in_list_table(browser, "1: Buy peacock feathers")
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
 
     # The page updates again, and now shows both items on her list
+    check_for_row_in_list_table(browser, "1: Buy peacock feathers")
+    check_for_row_in_list_table(browser, "2: Use peacock feathers to make a fly")
 
     # Edith wonders whether the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
@@ -58,3 +62,9 @@ def test_can_start_a_list_and_retrieve_it_later(browser):
     # She visits that URL - her to-do list is still there.
 
     # Satisfied, she goes back to sleep
+
+
+def check_for_row_in_list_table(browser, row_text):
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert row_text in [row.text for row in rows], f"New Item didn't appear in table {table.text}"
--- todolist/tests.py
+++ todolist/tests.py
@@ -9,3 +9,8 @@ from pytest_django.asserts import assertTemplateUsed
 def test_home_page_test(client):
     response = client.get('/')
     assertTemplateUsed(response, "home.html")
+
+
+def test_can_save_a_post(client):
+    response = client.post('/', data={'item_text': 'A new item added'})
+    assert 'A new item added' in response.content.decode('utf-8')
